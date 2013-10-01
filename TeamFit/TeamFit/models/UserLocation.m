ece259ac4d163942a090ce1b63743996161f008e
tenstriker <niravcp@gmail.com>
2013-09-30 21:30:46 -0700

    missign userlocation files

---
+++ TeamFit/TeamFit/models/UserLocation.m
@@ -0,0 +1,15 @@
+//
+//  UserLocation.m
+//  TeamFit
+//
+//  Created by Nirav Patel on 9/28/13.
+//  Copyright (c) 2013 abc. All rights reserved.
+//
+
+#import "UserLocation.h"
+
+@implementation UserLocation
+
+
+
+@end

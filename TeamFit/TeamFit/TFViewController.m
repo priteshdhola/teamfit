03ca54f2f9f85b5544badc2592c3c5e387c974f6
Pritesh Patel <pritesh@yummly.com>
2013-10-01 23:12:23 -0700

    login feature implemented

--- TeamFit/TeamFit/TFViewController.m
+++ TeamFit/TeamFit/TFViewController.m
@@ -45,80 +45,60 @@ NSInteger flag;
     [alertView show];
 }
 - (IBAction)loginClicked:(id)sender {
-    //    flag = 0;
-    //    NSLog(@"%@", [txtUsername text]);
-    //    NSLog(@"%@", [txtPassword text]);
-    //
-    //    if([[txtUsername text] isEqualToString:@"pritesh"] && [[txtPassword text] isEqualToString:@"patel"] ) {
-    //        [self alertStatus:@"Welcome" :@"Login Success!"];
-    //        flag=1;
-    //    } else {
-    //        [self alertStatus:@"Error" :@"Login Failed!"];
-    //    }
     
-    @try {
-        
-        if([[self.txtUsername text] isEqualToString:@""] || [[self.txtPassword text] isEqualToString:@""] ) {
-            [self alertStatus:@"Please enter both Username and Password" :@"Login Failed!"];
-        } else {
-            NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",[self.txtUsername text],[self.txtPassword text]];
-            NSLog(@"PostData: %@",post);
-            
-            //NSURL *url=[NSURL URLWithString:@"http://dipinkrishna.com/jsonlogin.php"];
-            NSURL *url=[NSURL URLWithString:@"http://localhost/~pritesh/jsonlogin.php"];
-            
-            
-            NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-            
-            NSString *postLength = [NSString stringWithFormat:@"%d", [postData length]];
-            
-            NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-            [request setURL:url];
-            [request setHTTPMethod:@"POST"];
-            [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-            [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-            [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-            [request setHTTPBody:postData];
-            
-            //[NSURLRequest setAllowsAnyHTTPSCertificate:YES forHost:[url host]];
-            
-            NSError *error = [[NSError alloc] init];
-            NSHTTPURLResponse *response = nil;
-            NSData *urlData=[NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-            
-            NSLog(@"Response code: %d", [response statusCode]);
-            if ([response statusCode] >=200 && [response statusCode] <300)
-            {
-                NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
-                NSLog(@"Response ==> %@", responseData);
-                
-                SBJsonParser *jsonParser = [SBJsonParser new];
-                NSDictionary *jsonData = (NSDictionary *) [jsonParser objectWithString:responseData error:nil];
-                NSLog(@"%@",jsonData);
-                NSInteger success = [(NSNumber *) [jsonData objectForKey:@"success"] integerValue];
-                NSLog(@"%d",success);
-                if(success == 1)
-                {
-                    NSLog(@"Login SUCCESS");
-                    [self alertStatus:@"Logged in Successfully." :@"Login Success!"];
-                    
-                } else {
-                    
-                    NSString *error_msg = (NSString *) [jsonData objectForKey:@"error_message"];
-                    [self alertStatus:error_msg :@"Login Failed!"];
-                }
-                
-            } else {
-                if (error) NSLog(@"Error: %@", error);
-                [self alertStatus:@"Connection Failed" :@"Login Failed!"];
-            }
-        }
-    }
-    @catch (NSException * e) {
-        NSLog(@"Exception: %@", e);
-        [self alertStatus:@"Login Failed." :@"Login Failed!"];
-    }
+    // Build JSON Object first
+    /***************************************************/
+    // make the inner dictionaries (probably would use a for loop for this)
+    NSDictionary *dict1 = [[NSDictionary alloc]
+                           initWithObjectsAndKeys:[self.txtUsername text],@"email",
+                           [self.txtPassword text],@"password",
+                           nil];
+    //NSDictionary *dict2 = [[NSDictionary alloc] initWithObjectsAndKeys:@"password", @"test", nil];
     
+    // put them in an array
+    //NSArray *types = [[NSArray alloc] initWithObjects:dict1, dict2, nil];
+    
+    // now put the array in a dictionary
+    NSDictionary *finalDict = [[NSDictionary alloc] initWithObjectsAndKeys:dict1, @"userData", nil];
+    NSError *error;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:finalDict options:0 error:&error];
+    
+    // Json string for debugging
+//    NSString *jsonString = [[NSString alloc] init];
+//    if (!jsonData) {
+//        NSLog(@"Got an error: %@", error);
+//    } else {
+//        jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+//    }
+    
+    /****************** Make the Post call for login ************************/
+    NSURL *locationOfWebService = [NSURL URLWithString:@"http://localhost:8080/tracksafe/users/auth"];
+    NSMutableURLRequest *theRequest = [[NSMutableURLRequest alloc]initWithURL:locationOfWebService];
+    NSHTTPURLResponse *response = nil;
+
+    [theRequest addValue:@"application/json" forHTTPHeaderField:@"Content-Type"];
+    [theRequest setHTTPMethod:@"POST"];
+    [theRequest setHTTPBody:jsonData];
+    
+    // Make the request and capture the data
+    NSData *urlData=[NSURLConnection sendSynchronousRequest:theRequest returningResponse:&response error:&error];
+    NSString *responseData = [[NSString alloc]initWithData:urlData encoding:NSUTF8StringEncoding];
+//    NSLog(@"Response code: %d", [response statusCode]);
+//    NSLog(@"Response ==> %@", responseData);
+    
+    
+    // If response is 200 than success
+    if ([response statusCode] ==200)
+    {
+        flag = 1;
+        NSLog(@"Login SUCCESS");
+        // This is custom segue call. This way segue only gets called when user enters correct username and password
+        // We also have to make sure that we connect UI story board to next story board via ctrl-drag and not the login button.
+        [self performSegueWithIdentifier:@"dashboard" sender:self];
+    } else {
+        if (error) NSLog(@"Login FAILURE: %@", error);
+        [self alertStatus:@"Please cheack your credentials" :@"Login Failed!"];
+    }
 }
 
 - (IBAction)backgroundClick:(id)sender {
@@ -128,13 +108,10 @@ NSInteger flag;
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    NSLog(@"%d",flag);
     if ([segue.identifier isEqualToString:@"dashboard"] && flag == 1) {
-        TFDashboardViewController * detailViewController = (TFDashboardViewController *) segue.destinationViewController;
-        //detailViewController.delegate = self;
-        //detailViewController.tipText = [self.txtUsername text];
+        UINavigationController *navigationController = segue.destinationViewController;
+        TFDashboardViewController *newActvitiyViewController = [[navigationController viewControllers] objectAtIndex:0];
     }
 }
-
 
 @end
